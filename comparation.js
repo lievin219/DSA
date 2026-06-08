4388d969788feee2bb504403e77114656881a97a
Gakiza Lievin <Gakiza@192.168.1.72>
2026-06-08 09:58:30 +0200

    skill: a dsa question trying to compare strings that occur more than one

---
+++ comparation.js
@@ -0,0 +1,29 @@
+//   Given a string s, count how many pairs of characters match when comparing from both ends.
+
+// Example 
+// Input:
+// s = "abccba"
+
+// Output:
+// 3
+
+function comparation(s){
+let left=0
+let right=s.length-1
+let sum=0
+while(left<right){
+    if(s[left]===s[right]){
+        sum=sum+1
+    }
+     left++
+     right--
+
+   
+}
+  
+console.log(sum)
+}
+
+comparation('abceyecba')
+
+
